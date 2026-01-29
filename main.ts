7fd602840846a37c0dbcb9d1d41719c57c2e266f
Pierce Bartine <piercebartine@gmail.com>
2026-01-28 21:24:25 -0800

    try hold-on-other-key-press

--- main.ts
+++ main.ts
@@ -1,33 +1,27 @@
 import { rule, writeToProfile, map } from "karabiner.ts";
 import { hrm, capsWord } from "karabiner.ts-greg-mods";
 
-writeToProfile(
-  "Default profile",
-  [
-    rule("Caps Lock to Escape").manipulators([map("caps_lock").to("escape")]),
-    capsWord().toggle(map("caps_lock", "shift").build()[0]).build(),
-    rule("Home row mods").manipulators(
-      hrm(
-        new Map([
-          ["a", "l⌃"],
-          ["s", "l⌥"],
-          ["d", "l⌘"],
-          ["f", "l⇧"],
-          ["j", "r⇧"],
-          ["k", "r⌘"],
-          ["l", "r⌥"],
-          [";", "r⌃"],
-        ]),
-      )
-        .lazy(true)
-        .holdTapStrategy("permissive-hold")
-        .chordalHold(true)
-        .simultaneousThreshold(90)
-        .tappingTerm(50)
-        .build(),
-    ),
-  ],
-  {
-    "basic.to_if_held_down_threshold_milliseconds": 50,
-  },
-);
+writeToProfile("Default profile", [
+  rule("Caps Lock to Escape").manipulators([map("caps_lock").to("escape")]),
+  capsWord().toggle(map("caps_lock", "shift").build()[0]).build(),
+  rule("Home row mods").manipulators(
+    hrm(
+      new Map([
+        ["a", "l⌃"],
+        ["s", "l⌥"],
+        ["d", "l⌘"],
+        ["f", "l⇧"],
+        ["j", "r⇧"],
+        ["k", "r⌘"],
+        ["l", "r⌥"],
+        [";", "r⌃"],
+      ]),
+    )
+      .lazy(true)
+      .holdTapStrategy("hold-on-other-key-press")
+      .chordalHold(true)
+      .simultaneousThreshold(90)
+      .tappingTerm(150)
+      .build(),
+  ),
+]);
