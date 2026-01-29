324346bbf00569092d16f0a98ba571179006ee5b
Pierce Bartine <piercebartine@gmail.com>
2026-01-28 20:25:55 -0800

    capsword

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,33 @@
-import { rule, writeToProfile } from "karabiner.ts";
-import { hrm } from "karabiner.ts-greg-mods";
+import { rule, writeToProfile, map } from "karabiner.ts";
+import { hrm, capsWord } from "karabiner.ts-greg-mods";
 
-// TODO: https://github.com/gregorias/karabiner.ts-greg-mods?tab=readme-ov-file#caps-word
-
-writeToProfile("Default profile", [
-  rule("Home row mods").manipulators(
-    hrm(
-      new Map([
-        ["a", "l⌃"],
-        ["s", "l⌥"],
-        ["d", "l⌘"],
-        ["f", "l⇧"],
-        ["j", "r⇧"],
-        ["k", "r⌘"],
-        ["l", "r⌥"],
-        [";", "r⌃"],
-      ]),
-    )
-      .lazy(true)
-      .holdTapStrategy("permissive-hold")
-      .chordalHold(true)
-      .simultaneousThreshold(90)
-      .tappingTerm(110)
-      .build(),
-  ),
-]);
+writeToProfile(
+  "Default profile",
+  [
+    rule("Caps Lock to Escape").manipulators([map("caps_lock").to("escape")]),
+    capsWord().toggle(map("caps_lock", "shift").build()[0]).build(),
+    rule("Home row mods").manipulators(
+      hrm(
+        new Map([
+          ["a", "l⌃"],
+          ["s", "l⌥"],
+          ["d", "l⌘"],
+          ["f", "l⇧"],
+          ["j", "r⇧"],
+          ["k", "r⌘"],
+          ["l", "r⌥"],
+          [";", "r⌃"],
+        ]),
+      )
+        .lazy(true)
+        .holdTapStrategy("permissive-hold")
+        .chordalHold(true)
+        .simultaneousThreshold(90)
+        .tappingTerm(110)
+        .build(),
+    ),
+  ],
+  {
+    "basic.to_if_held_down_threshold_milliseconds": 50,
+  },
+);
